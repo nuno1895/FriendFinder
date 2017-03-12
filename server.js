d7e9de930c93ae364d7c4b35509edc7b0d912d2a
Nuno Tavares <Nuno@iMac.local>
2017-03-12 17:06:35 -0400

    heroku build

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-var express = require("express");
+ var express = require("express");
 var bodyParser = require("body-parser");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
